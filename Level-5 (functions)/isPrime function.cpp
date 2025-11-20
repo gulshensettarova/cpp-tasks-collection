d1dff6bcbc136f9fde0780f2202bc0e5c46add2d
Gulshan Sattarova <90262999+gulshensettarova@users.noreply.github.com>
2025-11-20 09:08:36 +0400

    Added isPrime function

---
+++ Level-5 (functions)/isPrime function.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+bool isPrime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+          return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout<<"n=";
+    cin>>n;
+    isPrime(n) ? cout<<"Sade" : cout<<"Murekkeb";
+    return 0;
+}
